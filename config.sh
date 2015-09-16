79ff7c1a8cbe4fd531565eeec9a3eebcd7870fa6
Cameron Seader <cs@suse.com>
2015-09-16 09:40:34 -0600

    Added new options for config.sh to handle product,ssh,firewall,runlevel

--- config.sh
+++ config.sh
@@ -29,7 +29,26 @@ echo "Configure image: [$kiwi_iname]..."
 #======================================
 # SuSEconfig
 #--------------------------------------
+echo "** Running suseConfig..."
 suseConfig
+
+echo "** Running ldconfig..."
+/sbin/ldconfig
+
+#======================================
+# Setup baseproduct link
+#--------------------------------------
+suseSetupProduct
+
+#======================================
+# Setup default runlevel
+#--------------------------------------
+baseSetRunlevel 5 
+
+#======================================
+# Add missing gpg keys to rpm
+#--------------------------------------
+suseImportBuildKey
 
 #==========================================
 # setup gfxboot
@@ -54,11 +73,15 @@ baseCleanMount
 mkdir -p /var/lib/YaST2
 touch /var/lib/YaST2/reconfig_system
 
+# Enable sshd
+chkconfig sshd on
+
 #=====================================
 # Add repos 
 #-------------------------------------
-mkdir -p /etc/products.d
-ln -s /etc/products.d/SLES.prod /etc/products.d/baseproduct
+#mkdir -p /etc/products.d
+# fixed with above suseSetupProduct
+#ln -s /etc/products.d/SLES.prod /etc/products.d/baseproduct
 zypper ar -c -t yast2 -f '"iso:/?iso=/srv/iso/SUSE-Enterprise-Storage-2-DVD-x86_64-Build0034-Media1.iso"' '"SUSE Enterprise Storage 2"'
 zypper ar -c -t yast2 -f '"iso:/?iso=/srv/iso/SLE-12-Server-DVD-x86_64-GM-DVD1.iso"' '"SUSE Linux Enterprise Server 12"'
 zypper -n refresh 
@@ -74,12 +97,25 @@ su ceph -c 'ssh-keygen -P "" -f "/home/ceph/.ssh/id_rsa" -q'
 
 systemctl set-default -f graphical.target
 
-#=====================================
-# Add configuration options          
-#-------------------------------------
+#======================================
+# Sysconfig Update
+#--------------------------------------
+echo '** Update sysconfig entries...'
+baseUpdateSysConfig /etc/sysconfig/keyboard KEYTABLE us.map.gz 
+baseUpdateSysConfig /etc/sysconfig/network/config FIREWALL no
+baseUpdateSysConfig /etc/sysconfig/network/config NETWORKMANAGER no
+baseUpdateSysConfig /etc/sysconfig/console CONSOLE_FONT lat9w-16.psfu
+baseUpdateSysConfig /etc/sysconfig/displaymanager DISPLAYMANAGER xdm
+baseUpdateSysConfig /etc/sysconfig/windowmanager DEFAULT_WM icewm-session
 #sysconf_addword /etc/sysconfig/displaymanager DISPLAYMANAGER "gdm"
 #sysconf_addword /etc/sysconfig/windowmanager DEFAULT_WM "gnome"
-sed -i 's/DISPLAYMANAGER.*/DISPLAYMANAGER="gdm"/' /etc/sysconfig/displaymanager
-sed -i 's/DEFAULT_WM.*/DEFAULT_WM="gnome"/' /etc/sysconfig/windowmanager
+#sed -i 's/DISPLAYMANAGER.*/DISPLAYMANAGER="gdm"/' /etc/sysconfig/displaymanager
+#sed -i 's/DEFAULT_WM.*/DEFAULT_WM="gnome"/' /etc/sysconfig/windowmanager
+
+#======================================
+# SSL Certificates Configuration
+#--------------------------------------
+echo '** Rehashing SSL Certificates...'
+c_rehash
 
 exit 0
